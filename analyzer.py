67d74f4716b4b831c159358c82267761d0329263
tealcoderotis <parallelcircuit17@gmail.com>
2025-02-02 14:07:56 -0800

    Added many functions to the app
    - Added the ability to view a timeline of all the team's robot stops
    - Added the ability to see the type of each stop
    - Added the ability to load both from a CSV file and from SQL

--- analyzer.py
+++ analyzer.py
@@ -22,11 +22,19 @@ def getDatabaseData(host, user, password, database, table):
 def getTextValueFromDropdown(value, dropdownList):
     return dropdownList[value]
 
-def getDataFrame(host, user, password, database, table):
+def preprocessDataFrame(dataFrame):
+    dataFrame["robot_stop"] = dataFrame["robot_stop"].apply(getTextValueFromDropdown, args=(ROBOT_STOP_VALUES,))
+    dataFrame["robot_injure"] = dataFrame["robot_injure"].apply(getTextValueFromDropdown, args=(ROBOT_INJURE_VALUES,))
+    return dataFrame
+
+def getDataFrameFromDatabase(host, user, password, database, table):
     data = getDatabaseData(host, user, password, database, table)
     dataFrame = pandas.DataFrame(data[1], columns=data[0])
-    dataFrame["robot_stop"] = dataFrame["robot_stop"].apply(getTextValueFromDropdown, args=(ROBOT_STOP_VALUES))
-    return dataFrame
+    return preprocessDataFrame(dataFrame)
+
+def getDataFrameFromCSV(filePath):
+    dataFrame = pandas.read_csv(filePath)
+    return preprocessDataFrame(dataFrame)
 
 def getAllTeams(dataFrame):
     return dataFrame["team_number"].drop_duplicates().to_list()
@@ -34,12 +42,20 @@ def getAllTeams(dataFrame):
 def getDataFameForTeam(dataFrame, teamNumber):
     return dataFrame[dataFrame["team_number"].values == teamNumber]
 
-def getTotalRobotStops(dataFrame, teamNumber):
+def getTotalRobotStopsForEachType(dataFrame, teamNumber):
     teamDataFrame = getDataFameForTeam(dataFrame, teamNumber)
-    robotStops =  teamDataFrame[teamDataFrame["robot_stop"].values != 0]["robot_stop"].to_list()
-    return len(robotStops)
+    stopList = []
+    for i in range(1, len(ROBOT_STOP_VALUES)):
+        robotStops = teamDataFrame.loc[(dataFrame["robot_stop"] == ROBOT_STOP_VALUES[i])].shape[0]
+        stopList.append(robotStops)
+    injureList = []
+    for i in range(1, len(ROBOT_INJURE_VALUES)):
+        robotStops = teamDataFrame.loc[(dataFrame["robot_injure"] == ROBOT_INJURE_VALUES[i])].shape[0]
+        injureList.append(robotStops)
+    return [stopList, injureList]
 
 def getStopDetails(dataFrame, teamNumber):
     teamDataFrame = getDataFameForTeam(dataFrame, teamNumber)
-    robotStops =  teamDataFrame[teamDataFrame["robot_stop"].values != 0]["round_number", "robot_stop"].to_list()
-    return robotStops
+    teamDataFrame = teamDataFrame.sort_values(by=["timestamp"])
+    robotStops = teamDataFrame.loc[(dataFrame["robot_stop"] != ROBOT_STOP_VALUES[0]) | (dataFrame["robot_injure"]  != ROBOT_INJURE_VALUES[0])][["timestamp", "round_number", "robot_stop", "robot_injure"]]
+    return [robotStops.columns.values.tolist()] + robotStops.values.tolist()
--- app.py
+++ app.py
@@ -1,6 +1,50 @@
 import analyzer
 from PyQt5 import QtWidgets, QtCore, QtGui
 import sys
+import json
+
+class TeamLabel(QtWidgets.QWidget):
+    def __init__(self, teamNumber, robotStops, parent=None):
+        super().__init__(parent)
+        self.teamNumber = teamNumber
+        self.setMinimumHeight(50)
+        self.mainLayout = QtWidgets.QHBoxLayout()
+        self.setLayout(self.mainLayout)
+        if any(i != 0 for i in robotStops[0]) or any(i != 0 for i in robotStops[1]):
+            self.teamNumberLabel = QtWidgets.QLabel(text=f"{self.teamNumber} - ({", ".join(map(str, robotStops[0]))}) robot stops; ({", ".join(map(str, robotStops[1]))}) robot injures")
+        else:
+            self.teamNumberLabel = QtWidgets.QLabel(text=f"{self.teamNumber}")
+        self.mainLayout.addWidget(self.teamNumberLabel, stretch=1)
+        self.viewStopDetaisButton = QtWidgets.QPushButton(text="View timeline")
+        self.viewStopDetaisButton.clicked.connect(self.showStopDetails)
+        if any(i != 0 for i in robotStops[0]) or any(i != 0 for i in robotStops[1]):
+            self.mainLayout.addWidget(self.viewStopDetaisButton)
+
+    def showStopDetails(self):
+        mainWindow.showStopDetailsDialog(self.teamNumber)
+
+class StopDetailsDialog(QtWidgets.QDialog):
+    def __init__(self, teamNumber, data, parent=None):
+        super().__init__(parent)
+        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.mainTable = QtWidgets.QTableWidget()
+        self.mainTable.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
+        self.mainTable.setColumnCount(len(data[0]))
+        self.mainTable.setRowCount(len(data) - 1)
+        self.mainTable.setHorizontalHeaderLabels(data[0])
+        for row in range(1, len(data)):
+            for column in range(len(data[row])):
+                self.mainTable.setItem(row - 1, column, QtWidgets.QTableWidgetItem(str(data[row][column])))
+        self.mainLayout.addWidget(self.mainTable, stretch=1)
+        self.buttonBox = QtWidgets.QDialogButtonBox()
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.accepted.connect(self.accept)
+        self.mainLayout.addWidget(self.buttonBox)
+        self.setWindowModality(True)
+        self.setWindowTitle(f"{teamNumber}'s Robot Stops and Injures")
+        self.setMinimumSize(500, 300)
+        self.show()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -13,15 +57,40 @@ class MainWindow(QtWidgets.QMainWindow):
         self.mainWidget.setLayout(self.mainLayout)
         self.mainScrollArea.setWidget(self.mainWidget)
         self.setCentralWidget(self.mainScrollArea)
-        self.dataFrame = analyzer.getDataFrame("localhost", "root", "orfscouting789", "scouting_database", "default")
+        self.setMinimumSize(400, 200)
+        self.setGeometry(100, 50, 400, 600)
+        self.show()
+        file = open("config.json", "r")
+        config = json.loads(file.read())
+        file.close()
+        databaseSucessful = False
+        if config["useDatabase"] == True:
+            try:
+                self.dataFrame = analyzer.getDataFrameFromDatabase(config["databaseInfo"]["host"], config["databaseInfo"]["user"], config["databaseInfo"]["password"], config["databaseInfo"]["database"], config["databaseInfo"]["table"])
+            except:
+                result = QtWidgets.QMessageBox.critical(self,  "Cannot Connect to Database", "A database connection cannot be established. Do you want to use a CSV file instead?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+                if result != QtWidgets.QMessageBox.Yes:
+                    sys.exit()
+            else:
+                databaseSucessful = True
+        if not databaseSucessful:
+            filePath = QtWidgets.QFileDialog.getOpenFileName(self, filter="CSV files (*.csv)")[0]
+            if filePath != "":
+                self.dataFrame = analyzer.getDataFrameFromCSV(filePath)
+            else:
+                sys.exit()
         teams = analyzer.getAllTeams(self.dataFrame)
         for team in teams:
             self.addTeam(team)
-        self.show()
-
+        
     def addTeam(self, teamNumber):
-        teamLabel = QtWidgets.QLabel(text=f"{teamNumber} - {analyzer.getTotalRobotStops(self.dataFrame, teamNumber)} - {analyzer.getStopDetails(self.dataFrame, teamNumber)}")
+        teamLabel = TeamLabel(teamNumber, analyzer.getTotalRobotStopsForEachType(self.dataFrame, teamNumber))
         self.mainLayout.insertWidget(self.mainLayout.count() - 1, teamLabel)
+
+    def showStopDetailsDialog(self, teamNumber):
+        data = analyzer.getStopDetails(self.dataFrame, teamNumber)
+        dialog = StopDetailsDialog(teamNumber, data, self)
+        dialog.exec()
 
 app = QtWidgets.QApplication(sys.argv)
 mainWindow = MainWindow()
